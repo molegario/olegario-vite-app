f761f74ef370b897b520874e903da03e5d26ab3a
molegario <molegario@x2omedia.com>
2024-03-19 07:05:54 -0400

    added processing detection/indication on form submit

--- src/routes/NewPost.tsx
+++ src/routes/NewPost.tsx
@@ -1,24 +1,45 @@
 import classes from "./NewPost.module.scss";
+import classNames from "classnames";
 import Modal from "../components/Modal";
-import { Link, Form } from "react-router-dom";
-import { LoaderFunctionArgs, redirect } from 'react-router-dom';
+import { Link, Form, ActionFunctionArgs, redirect } from "react-router-dom";
+import { useCallback, useState } from "react";
+// import { LoaderFunctionArgs, redirect } from 'react-router-dom';
 
 function NewPost() {
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleSubmitClick = useCallback(
+    ()=>{
+      setIsProcessing(true);
+    },
+    [setIsProcessing]
+  );
 
   return (
-    <Modal >
-      <Form method='post' className={classes.form}>
+    <Modal>
+      <Form 
+        method='post' 
+        className={
+          classNames(
+            classes.form, 
+            {
+              [`${classes.processing}`]: isProcessing
+            }
+          )  
+        } 
+        onSubmit={handleSubmitClick}
+      >
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" name="body" required rows={3}></textarea>
+          <textarea id="body" name="body" required rows={3} disabled={isProcessing}></textarea>
         </p>
         <p>
           <label htmlFor="name">Your Name</label>
-          <input type="text" id="name" name="author" required />
+          <input type="text" id="name" name="author" disabled={isProcessing} required />
         </p>
         <p className={classes.actions}>
           <Link to='..'>Cancel</Link>
-          <button type="submit">Add Post</button>
+          <button type="submit" disabled={isProcessing}>Add Post</button>
         </p>
       </Form>
     </Modal>
@@ -27,7 +48,7 @@ function NewPost() {
 
 export default NewPost;
 
-export async function action({ request }: LoaderFunctionArgs) {
+export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const postData = Object.fromEntries(formData);
   await fetch('http://localhost:8080/posts', {
